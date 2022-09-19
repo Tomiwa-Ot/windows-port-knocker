5ad8842041f5999422615e2def09b1fd24822a05
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-09-19 13:51:59 +0100

    updated docs in port-knocker.ps1

--- port-knocker.ps1
+++ port-knocker.ps1
@@ -3,7 +3,11 @@
     Lock/Unlock ports by knocking closed ports
 
 .DESCRIPTION
-    A longer description.
+    Enable/Disable ports by sending a sequence of SYN packets
+    to pre-defined ports. The Knockd service checks the windows
+    firewall logs to see if SYN packets were sequentially sent 
+    to the defined ports within a space of 10 seconds from the 
+    same IP address and then, enable/disable a port.
 
 .EXAMPLE
     .\port-knocker.ps1
