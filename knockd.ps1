fd8068f95ee02acf4cebc110f325e299d3f35f1a
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-10-17 18:09:47 +0100

    made some additions

--- knockd.ps1
+++ knockd.ps1
@@ -16,6 +16,9 @@
     https://github.com/Tomiwa-Ot/windows-port-knocker
 #>
 
+$FirewallLogs = "$($Env:SystemRoot)\system32\LogFiles\Firewall\pfirewall.log"
+$IP = $null
+
 Function Write-Toast {
     param(
         [Parameter(Mandatory)]
@@ -47,5 +50,87 @@ Function Write-Toast {
     [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($LauncherId).Show($ToastMessage)
 }
 
-$firewallLogs = "$($Env:SystemRoot)\system32\LogFiles\Firewall\pfirewall.log"
-$timeout = 10
+
+Function Watch-Logs {
+    param()
+
+    $Timeout = 10 
+
+    $LogPath = Split-Path $FirewallLogs -Parent
+    $LogFile = Split-Path $FirewallLogs -Leaf
+    $Watcher = New-Object IO.FileSystemWatcher $LogPath, $LogFile -Property @{
+        IncludeSubdirectories = $false
+        EnableRaisingEvents = $true
+    }
+    
+    Register-ObjectEvent $Watcher -SourceIdentifier 'Knockd.LogWatcher' -Action {
+        $LogsLastLine = Get-Content $FirewallLogs -Tail 1
+        $global:IP = $LogsLastLine.Split(' ')[4]
+        $global:Protocol = $LogsLastLine.Split(' ')[3]
+        $DestinationPort = $LogsLastLine.Split(' ')[7]
+        $global:json = Get-Content '.\data.json' | ConvertFrom-Json
+        foreach ($global:object in $global:json.Daemon) {
+            if ($global:object.OpenSequence.Split(' ')[0] -eq $DestinationPort -and $global:object.Protocol -eq $Protocol) {
+                Register-ObjectEvent $Watcher -SourceIdentifier 'Knockd.OpenSeq1' -Action {
+                    $LogsLastLine2 = Get-Content $FirewallLogs -Tail 1
+                    $IP2 = $LogsLastLine2.Split(' ')[4]
+                    $Protocol2 = $LogsLastLine2.Split(' ')[3]
+                    $DestinationPort2 = $LogsLastLine2.Split(' ')[7]
+                    if ($global:object.OpenSequence.Split(' ')[1] -eq $DestinationPort2 -and $IP2 -eq $global:IP -and $global:Protocol -eq $Protocol2) {
+                        Register-ObjectEvent $Watcher -SourceIdentifier 'Knockd.OpenSeq2' -Action {
+                            $LogsLastLine3 = Get-Content $FirewallLogs -Tail 1
+                            $IP3 = $LogsLastLine3.Split(' ')[4]
+                            $Protocol3 = $LogsLastLine3.Split(' ')[3]
+                            $DestinationPort3 = $LogsLastLine3.Split(' ')[7]
+                            if ($global:object.OpenSequence.Split(' ')[1] -eq $DestinationPort3 -and $IP3 -eq $global:IP -and $global:Protocol -eq $Protocol3) {
+                                if ($global:Protocol -eq 'TCP') {
+                                    Start-Service (Get-Process -Id (Get-NetTCPConnection -LocalPort $Port).OwningProcess).ProcessName
+                                    Write-Toast -Message "Knockd started $((Get-Process -Id (Get-NetTCPConnection -LocalPort $Port).OwningProcess).ProcessName) service $($global:object.Port)/$($global:Protocol)"
+                                }
+                                if($global:Protocol -eq 'UDP') {
+                                    Start-Service (Get-Process -Id (Get-NetUDPEndpoint -LocalPort $Port).OwningProcess).ProcessName
+                                    Write-Toast -Message "Knockd started $((Get-Process -Id (Get-NetUDPEndpoint -LocalPort $Port).OwningProcess).ProcessName) service $($global:object.Port)/$($global:Protocol)"
+                                }
+                            }
+                        }
+                        Wait-Event -SourceIdentifier 'Knockd.OpenSeq2' -Timeout $Timeout
+                    }
+                }
+                Wait-Event -SourceIdentifier 'Knockd.OpenSeq1' -Timeout $Timeout
+            }
+            if($global:object.CloseSequence.Split(' ')[0] -eq $DestinationPort -and $global:object.Protocol -eq $Protocol) {
+                Register-ObjectEvent $Watcher -SourceIdentifier 'Knockd.CloseSeq1' -Action {
+                    $LogsLastLine2 = Get-Content $FirewallLogs -Tail 1
+                    $IP2 = $LogsLastLine2.Split(' ')[4]
+                    $Protocol2 = $LogsLastLine2.Split(' ')[3]
+                    $DestinationPort2 = $LogsLastLine2.Split(' ')[7]
+                    if ($global:object.OpenSequence.Split(' ')[1] -eq $DestinationPort2 -and $IP2 -eq $global:IP -and $global:Protocol -eq $Protocol2) {
+                        Register-ObjectEvent $Watcher -SourceIdentifier 'Knockd.CloseSeq2' -Action {
+                            $LogsLastLine3 = Get-Content $FirewallLogs -Tail 1
+                            $IP3 = $LogsLastLine3.Split(' ')[4]
+                            $Protocol3 = $LogsLastLine3.Split(' ')[3]
+                            $DestinationPort3 = $LogsLastLine3.Split(' ')[7]
+                            if ($global:object.OpenSequence.Split(' ')[1] -eq $DestinationPort3 -and $IP3 -eq $global:IP -and $global:Protocol -eq $Protocol3) {
+                                if ($global:Protocol -eq 'TCP') {
+                                    Stop-Service (Get-Process -Id (Get-NetTCPConnection -LocalPort $Port).OwningProcess).ProcessName
+                                    Write-Toast -Message "Knockd stopped $((Get-Process -Id (Get-NetTCPConnection -LocalPort $Port).OwningProcess).ProcessName) service $($global:object.Port)/$($global:Protocol)"
+                                }
+                                if($global:Protocol -eq 'UDP') {
+                                    Stop-Service (Get-Process -Id (Get-NetUDPEndpoint -LocalPort $Port).OwningProcess).ProcessName
+                                    Write-Toast -Message "Knockd stopped $((Get-Process -Id (Get-NetUDPEndpoint -LocalPort $Port).OwningProcess).ProcessName) service $($global:object.Port)/$($global:Protocol)"
+                                }
+                            }
+                        }
+                        Wait-Event -SourceIdentifier 'Knockd.CloseSeq2' -Timeout $Timeout
+                    }
+                }
+                Wait-Event -SourceIdentifier 'Knockd.CloseSeq1' -Timeout $Timeout
+            }
+        }
+    }
+    
+}
+
+while ($true) {
+    Watch-Logs
+}
