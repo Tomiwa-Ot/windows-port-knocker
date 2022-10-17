95d675d4d5bd3b619b837f55b64497fe20777e75
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-10-17 14:29:19 +0100

    added toast notification function

--- knockd.ps1
+++ knockd.ps1
@@ -16,5 +16,36 @@
     https://github.com/Tomiwa-Ot/windows-port-knocker
 #>
 
+Function Write-Toast {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Message
+    )
+
+    # Powershell Launcher ID
+    $LauncherId = (Get-StartApps | Where-Object -Property Name -eq "Windows Powershell").AppID
+
+    # Load windows toast assemblies
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+    [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+
+    # Xml template
+    [xml]$ToastTemplate = @"
+        <toast>
+            <visual>
+                <binding template="ToastImageAndText03">
+                    <text id="1">Port Knocker</text>
+                    <text id="2">$($Message)</text>
+                </binding>
+            </visual>
+        </toast>
+"@
+
+    $ToastXml = [Windows.Data.Xml.Dom.XmlDocument]::New()
+    $ToastXml.LoadXml($ToastTemplate.OuterXml)
+    $ToastMessage = [Windows.UI.Notifications.ToastNotification]::New($ToastXml)
+    [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($LauncherId).Show($ToastMessage)
+}
+
 $firewallLogs = "$($Env:SystemRoot)\system32\LogFiles\Firewall\pfirewall.log"
 $timeout = 10
